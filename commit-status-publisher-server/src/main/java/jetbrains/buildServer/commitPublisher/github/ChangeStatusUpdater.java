8d5e17538e5b31ea8f4e433612589910163cc951
manhtran1609 <manh.tran@logigear.com>
2017-07-11 09:11:39 +0700

    tolo bivcl

--- commit-status-publisher-server/src/main/java/jetbrains/buildServer/commitPublisher/github/ChangeStatusUpdater.java
+++ commit-status-publisher-server/src/main/java/jetbrains/buildServer/commitPublisher/github/ChangeStatusUpdater.java
@@ -207,7 +207,7 @@ public class ChangeStatusUpdater {
             comment.append(getViewResultsUrl(build));
             comment.append(") ");
 
-            comment.append("1343outcomdd   fffbvnbnbshgf  fdghdfgahidgfd cgbfczavsllffl **").append(status.getState().toUpperCase()).append("**\n");
+            comment.append("1343outcomdd   fffbvnbnbshgf  fdghdfgahidgfd cgbfczav **").append(status.getState().toUpperCase()).append("**\n");
 
             final String text = status.getState();
             if (text != null) {
