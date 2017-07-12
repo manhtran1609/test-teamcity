8c7acf0804643e03f24d811cf5bea9d8ad41f4af
manhtran1609 <manh.tran@logigear.com>
2017-07-12 10:42:53 +0700

    sfdsdg

--- commit-status-publisher-server/src/main/java/jetbrains/buildServer/commitPublisher/github/ChangeStatusUpdater.java
+++ commit-status-publisher-server/src/main/java/jetbrains/buildServer/commitPublisher/github/ChangeStatusUpdater.java
@@ -207,7 +207,7 @@ public class ChangeStatusUpdater {
             comment.append(getViewResultsUrl(build));
             comment.append(") ");
 
-            comment.append("1343outcomdd   fffbvnbnbshfgf  fdghddcddfgahidgfd cgbfczgf **").append(status.getState().toUpperCase()).append("**\n");
+            comment.append("1343outcomdd   fffbvnbnbshfgf  fdghddsscddfgahidgfd cgbfczgf **").append(status.getState().toUpperCase()).append("**\n");
 
             final String text = status.getState();
             if (text != null) {
