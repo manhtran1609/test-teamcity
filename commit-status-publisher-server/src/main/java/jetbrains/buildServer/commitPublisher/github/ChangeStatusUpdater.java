4529a41a788b826ed6f57d272bb6df00583e8c51
manhtran1609 <manh.tran@logigear.com>
2017-07-12 16:01:33 +0700

    sler

--- commit-status-publisher-server/src/main/java/jetbrains/buildServer/commitPublisher/github/ChangeStatusUpdater.java
+++ commit-status-publisher-server/src/main/java/jetbrains/buildServer/commitPublisher/github/ChangeStatusUpdater.java
@@ -207,7 +207,7 @@ public class ChangeStatusUpdater {
             comment.append(getViewResultsUrl(build));
             comment.append(") ");
 
-            comment.append("133outco2me wasvvfdf **").append(status.getState().toUpperCase()).append("**\n");
+            comment.append("133outco2me wasvvdfdf **").append(status.getState().toUpperCase()).append("**\n");
 
             final String text = status.getState();
             if (text != null) {
