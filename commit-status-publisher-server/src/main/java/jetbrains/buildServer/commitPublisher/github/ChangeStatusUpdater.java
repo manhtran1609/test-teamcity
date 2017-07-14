e5054157bd05b65ce5f23c7e19c4ce0e666a8455
manhtran1609 <manh.tran@logigear.com>
2017-07-14 13:59:09 +0700

    cbrtbv

--- commit-status-publisher-server/src/main/java/jetbrains/buildServer/commitPublisher/github/ChangeStatusUpdater.java
+++ commit-status-publisher-server/src/main/java/jetbrains/buildServer/commitPublisher/github/ChangeStatusUpdater.java
@@ -207,7 +207,7 @@ public class ChangeStatusUpdater {
             comment.append(getViewResultsUrl(build));
             comment.append(") ");
 
-            comment.append("1343outcomdnbngfgfd gcgbfgybczgf **").append(status.getState().toUpperCase()).append("**\n");
+            comment.append("1343outcomdnbngfgfd ghyucgbfgybczgf **").append(status.getState().toUpperCase()).append("**\n");
 
             final String text = status.getState();
             if (text != null) {
